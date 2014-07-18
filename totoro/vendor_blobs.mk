f2702259d97304ee9f63fa9ad5bd98ab15684257
Conn O'Griofa <connogriofa@gmail.com>
2014-07-18 22:29:48 +0100

    Cleanup bcm21553 proprietaries
    Also install libcamera.so on cooperve to fix camera.
    
    Change-Id: I7861cbeb8552c0a9714abd29dc1b49ef9d831783

--- totoro/vendor_blobs.mk
+++ totoro/vendor_blobs.mk
@@ -14,24 +14,10 @@
 
 # Camera
 PRODUCT_COPY_FILES += \
-    vendor/samsung/totoro/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-    vendor/samsung/totoro/proprietary/lib/libbrcmjpeg.so:system/lib/libbrcmjpeg.so \
-#    vendor/samsung/totoro/proprietary/lib/libarccamera.so:system/lib/libarccamera.so \
-#    vendor/samsung/totoro/proprietary/lib/libCaMotion.so:system/lib/libCaMotion.so \
-#    vendor/samsung/totoro/proprietary/lib/libPanoraMax1.so:system/lib/libPanoraMax1.so \
-#    vendor/samsung/totoro/proprietary/lib/libseccamera.so:system/lib/libseccamera.so \
-#    vendor/samsung/totoro/proprietary/lib/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
-#    vendor/samsung/totoro/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
-#    vendor/samsung/totoro/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
-#    vendor/samsung/totoro/proprietary/lib/libcaps.so:system/lib/libcaps.so
+    vendor/samsung/totoro/proprietary/lib/libcamera.so:system/lib/libcamera.so
 
 # Sensors
 PRODUCT_COPY_FILES += \
     vendor/samsung/totoro/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so
-#    vendor/samsung/totoro/proprietary/lib/hw/acoustics.default.so:system/lib/hw/acoustics.default.so \
-#    vendor/samsung/totoro/proprietary/lib/libaccsensorcal.so:system/lib/libaccsensorcal.so \
-#    vendor/samsung/totoro/proprietary/lib/libaccsensorcaltest.so:system/lib/libaccsensorcaltest.so \
-#    vendor/samsung/totoro/proprietary/lib/libaccelcal.so:system/lib/libaccelcal.so \
-#    vendor/samsung/totoro/proprietary/lib/libdhwr.so:system/lib/libdhwr.so
 
 $(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
